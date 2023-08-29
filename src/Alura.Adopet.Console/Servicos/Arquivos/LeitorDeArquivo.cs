55d1c2ed23f1cb548152dbd13d37057f889d064f
Daniel Portugal <dpcosta@gmail.com>
2023-08-29 13:49:44 -0300

    Ajustes para remover a classe PetAPartirDoCsv

--- src/Alura.Adopet.Console/Servicos/Arquivos/LeitorDeArquivo.cs
+++ src/Alura.Adopet.Console/Servicos/Arquivos/LeitorDeArquivo.cs
@@ -16,14 +16,25 @@ public class LeitorDeArquivo
         {
             return null;
         }
-        List<Pet> listaDePet = new List<Pet>();
+        List<Pet> lista = new List<Pet>();
         using StreamReader sr = new StreamReader(caminhoDoArquivoASerLido);
         while (!sr.EndOfStream)
         {
             string? linha = sr.ReadLine();
             if (linha is not null)
-                listaDePet.Add(linha.ConverteDoTexto());
+            {
+                string[] propriedades = linha.Split(';');
+                bool guidValido = Guid.TryParse(propriedades[0], out Guid petId);
+                if (!guidValido) throw new ArgumentException("Identificador do pet inválido!");
+
+                bool tipoValido = int.TryParse(propriedades[2], out int tipoPet);
+                if (!tipoValido) throw new ArgumentException("Tipo do pet inválido!");
+
+                TipoPet tipo = tipoPet == 1 ? TipoPet.Gato : TipoPet.Cachorro;
+
+                lista.Add(new Pet(petId, propriedades[1], tipo));
+            }
         }
-        return listaDePet;
+        return lista;
     }
 }
