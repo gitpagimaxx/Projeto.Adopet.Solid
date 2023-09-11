dda7280663295f1c83e416ff5ebe2ca7ac0e5c9e
Daniel Portugal <dpcosta@gmail.com>
2023-09-11 16:57:17 -0300

    Ajuste nas variáveis na fábrica de comandos.

--- src/Alura.Adopet.Console/Comandos/ComandosFactory.cs
+++ src/Alura.Adopet.Console/Comandos/ComandosFactory.cs
@@ -15,18 +15,22 @@ public static class ComandosFactory
         switch (comando)
         {
             case "import":
-            var httpClientPet = new HttpClientPet(new AdopetAPIClientFactory().CreateClient("adopet"));
-            LeitorDeArquivo leitorDeArquivos = new(argumentos[1]);
-                return new Import(httpClientPet, leitorDeArquivos);                    
+                var httpClientPet = new HttpClientPet(new AdopetAPIClientFactory().CreateClient("adopet"));
+                var leitorDeArquivos = new LeitorDeArquivo(argumentos[1]);
+                return new Import(httpClientPet, leitorDeArquivos);
+
             case "list":
                 var httpClientPetList = new HttpClientPet(new AdopetAPIClientFactory().CreateClient("adopet"));
-                return new List(httpClientPetList);               
+                return new List(httpClientPetList);
+
             case "show":
-                LeitorDeArquivo leitorDeArquivosShow = new(argumentos[1]);
+                var leitorDeArquivosShow = new LeitorDeArquivo(argumentos[1]);
                 return new Show(leitorDeArquivosShow);
+
             case "help":
                 var comandoASerExibido = argumentos.Length==2? argumentos[1] : null;
                 return new Help(comandoASerExibido);
+
             default: return null;
         }           
     }
